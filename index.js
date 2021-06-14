9167b889d8a8a16b180d266f2cd527046d487538
KyleFeller <76542408+Fe1ky@users.noreply.github.com>
2021-06-14 21:42:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,127 @@
+// Code your selectRandomEntry function here:
+function selectRandomEntry(array){
+   let index = Math.floor(Math.random()*array.length);
+   return array[index];
+}
+
+
+
+
+
+// Code your buildCrewArray function here:
+
+
+let idNumbers = [291, 414, 503, 599, 796, 890];
+ let newCrew = [] 
+
+
+
+function buildCrewArray(ids, candidates){ 
+ 
+  let threeCandidates=[]
+  while(threeCandidates.length<3){
+  let item = selectRandomEntry(ids);
+if(!threeCandidates.includes(item)){
+  threeCandidates.push(item);
+  }
+}
+
+  for (i=0; i < threeCandidates.length; i++){ 
+    for (j=0; j<candidates.length; j++){
+       if (candidates[j].astronautID === threeCandidates[i])
+       { newCrew.push(candidates[j]); 
+       } 
+     } 
+       } 
+       return newCrew;
+ } 
+   
+
+
+
+// function buildCrewArray(ids,animal){
+
+// while(threeCandidates.length<3){
+//   let item = selectRandomEntry(ids);
+// if(!threeCandidates.includes(item)){
+//   threeCandidates.push(item);
+//   }
+// }
+// return threeCandidates;
+
+// }
+//  newCrew = [] 
+//   for (i=0; i < threeCandidates.length; i++){ 
+//     for (j=0; j<animals.length; j++){
+//        if (animals[j].astronautID === threeCandidates[i]){
+//           newCrew.push(animals[j]); 
+//        } 
+//      } 
+//     } 
+
+// while(astronauts<3){
+
+//   for(let j=0; j<threeCandidates.length;j++){
+//     if (animal[j].astronautID === threeCandidates[i]){
+//       astronauts.push(animal[j].name);
+//     }
+//   }
+// }
+// return astronauts
+// }
+
+
+
+
+
+
+// Here are the candidates and the 'animals' array:
+let candidateA = {
+  'name':'Gordon Shumway',
+  'species':'alf',
+  'mass':90,
+  'o2Used':function(hrs){return 0.035*hrs},
+  'astronautID':414
+};
+let candidateB = {
+  'name':'Lassie',
+  'species':'dog',
+  'mass':19.1,
+  'o2Used':function(hrs){return 0.030*hrs},
+  'astronautID':503
+};
+let candidateC = {
+  'name':'Jonsey',
+  'species':'cat',
+  'mass':3.6,
+  'o2Used':function(hrs){return 0.022*hrs},
+  'astronautID':796
+};
+let candidateD = {
+  'name':'Paddington',
+  'species':'bear',
+  'mass':31.8,
+  'o2Used':function(hrs){return 0.047*hrs},
+  'astronautID':291
+};
+let candidateE = {
+  'name':'Pete',
+  'species':'tortoise',
+  'mass':417,
+  'o2Used':function(hrs){return 0.010*hrs},
+  'astronautID':599
+};
+let candidateF = {
+  'name':'Hugs',
+  'species':'ball python',
+  'mass':2.3,
+  'o2Used':function(hrs){return 0.018*hrs},
+  'astronautID':890
+};
+
+let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+
+// Code your template literal and console.log statements:
+
+buildCrewArray(idNumbers,animals)
+console.log(`${newCrew[0].name},${newCrew[1].name}, and ${newCrew[2 ].name} are going to space!`);
